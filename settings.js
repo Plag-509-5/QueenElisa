3d996ac0e7fc82c173600808ee1968217e7afd06
Plag-509-5 <camav1260@gmail.com>
2024-12-03 17:45:11 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QQ2UHMi1kZI5GTXFVV4tWWS9Wejp2doRDNDZ0VzN2apB3dllGbONmS4YESjIWciFzUOFFOpwd=96804474905',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
